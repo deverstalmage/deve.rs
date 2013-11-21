ea92788eea9f77c2615d40c0c29a19c805819779
Devers Talmage <deverstalmage@gmail.com>
2013-11-21 17:08:45 -0500

    worked on resume section

--- app/scripts/services/resume.js
+++ app/scripts/services/resume.js
@@ -4,11 +4,16 @@ angular.module('deve.rsApp')
     .service('Resume', function Resume() {
         return {
             getTechSkills: function() {
-                return [];
+                return [
+                    'Javascript (jQuery, AngularJS)',
+                    'PHP (WordPress)',
+                    'HTML & CSS (Bootstrap, Foundation, Sass)'
+                ];
             },
             getExpertise: function() {
             },
             getEducation: function() {
+                return ['BS Computer Science &mdash; University of Massachusetts, Amherst &mdash; 2008-2011'];
             },
             getExperience: function() {
             }
